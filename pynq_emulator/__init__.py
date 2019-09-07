18636bc1cf4979a0cbb9b87f906f1cf76fc38387
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2019-09-07 19:59:28 +0300

    Adding licence, renaming files

---
+++ pynq_emulator/__init__.py
@@ -0,0 +1,1 @@
+name = "pynq_emulator"
--- setup.py
+++ setup.py
@@ -9,15 +9,16 @@ setup(
     description="An emulator for TUNI students' PYNQ exercises",
     author='Riku Salminen',
     author_email='riku.salminen@tuni.fi',
-    packages=['pynq_emulator'],  #same as name
-    install_requires=[], #external packages as dependencies
+    packages=['pynq_emulator'],  # same as name
+    install_requires=[],  # external packages as dependencies
     scripts=[],
     url="https://github.com/Riku-S/led-matrix",
-    download_url="https://github.com/Riku-S/led-matrix/archive/master.zip"
-	
-	classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
+    download_url="https://github.com/Riku-S/led-matrix/archive/master.zip",
+
+    classifiers = [
+                  'Programming Language :: Python :: 3.5',
+                  'Programming Language :: Python :: 3.6',
+                  'Programming Language :: Python :: 3.7',
+              ],
+    python_requires='>=3.6',
 )
