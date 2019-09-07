9c3588d783fa12bb4919196358b69d16ffffb72d
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2019-09-07 22:37:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+with open("requirements.txt", 'r') as f:
+    requires = f.read().split()
+
 setuptools.setup(
     name='pynq_emulator',
     version='1.2',
