09aad2e336e9d3b1e53e0d6664e7fede274ef443
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2019-09-07 22:38:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", 'r') as f:
 
 setuptools.setup(
     name='pynq_emulator',
-    version='1.2',
+    version='1.3',
     author='Riku Salminen',
     author_email='riku.salminen@tuni.fi',
     description="An emulator for TUNI students' PYNQ exercises",
