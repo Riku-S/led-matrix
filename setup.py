408bbc205d216fa38a1a4c46ead70e2849761b06
Riku Salminen <38985578+Riku-S@users.noreply.github.com>
2019-09-08 09:06:33 +0300

    setup updates

--- setup.py
+++ setup.py
@@ -4,27 +4,30 @@ import setuptools
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r') as f:
-    requires = f.read().split()
-
 setuptools.setup(
     name='pynq_emulator',
-    version='1.12',
+    version='1.13',
     author='Riku Salminen',
     author_email='riku.salminen@tuni.fi',
     description="An emulator for TUNI students' PYNQ exercises",
 	long_description = long_description,
     packages=setuptools.find_packages(exclude=[]),
+    package_data = {},
+	keywords='pynq emulator',
     scripts=[],
     url="https://github.com/Riku-S/led-matrix",
     download_url="https://github.com/Riku-S/led-matrix/archive/master.tar.gz",
-    install_requires=requires,
-    classifiers = [
+    install_requires=[],
+    classifiers = ["Development Status :: Beta 1",
+                   "Natural Language :: English",
+                   "Intended Audience :: TUNI Students",
 				   'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
 				   ],
-    python_requires='>=2.7',
+	entry_points="""
+	# -*- Entry points: -*-
+    """,
 )
